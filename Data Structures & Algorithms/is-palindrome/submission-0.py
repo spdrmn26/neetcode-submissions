a048e1bc83d8979a0bbbf3466d724226a5f68155
Ambika Banerjee <ambikabanerjee17@gmail.com>
2026-06-30 10:29:09 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # two pointer problem
+        # since here a palindrome can be a sentence as well
+        # with first letter as capital and last as small
+        # eg: Input: s = "Was it a car or a cat I saw?"
+        # Output: true
+        # (ord(s[i]) - ord(s[j]) == 32)): 32 is the difference
+        # between Capital and Small ASCII alphabets
+        i, j = 0, len(s) - 1
+        while i < j:
+            # skip if alpha-numeric:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() == s[j].lower():
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
